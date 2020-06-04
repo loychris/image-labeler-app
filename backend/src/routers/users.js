e0151435c009920f26c223d60d99e20490d10432
Martin <l.martin.a@hotmail.com>
2020-06-04 19:32:41 +0200

    made empty routes for user: controlers are to be added later? not sure if i understood task correctly require someone to tell me whether and how far i have understood/completed the given task.

--- backend/src/routers/users.js
+++ backend/src/routers/users.js
@@ -1,10 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
+const User = require("../models/user");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("sign_up", function (req, res) {
+  //todo
+});
+
+router.post("sign_in", function (req, res) {
+  //todo
+});
+
+router.get("/labels", function (req, res) {
+  //todo
+});
+
+router.post("/pictures", function (req, res) {
+  //todo
+});
+router.get("image", function (req, res) {
+  //todo
+});
+
+router.get("/labeling", function (req, res) {
+  //not in our todos but this is the route to actualy get pictures to be labeled
 });
 
 module.exports = router;
