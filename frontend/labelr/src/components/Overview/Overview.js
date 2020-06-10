91a56bc02c76e1e78c7e59703f5a1edbecfbd7c5
morixn <64641295+morixn@users.noreply.github.com>
2020-06-10 19:28:33 +0200

    SetUp Menupage

--- frontend/labelr/src/components/Overview/Overview.js
+++ frontend/labelr/src/components/Overview/Overview.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import {Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+// import { faHome } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import CatPreview from './CatPreview/CatPreview';
 
 import classes from './Overview.module.css';
 
 
+
+
 class Overview extends Component {
 
     state = {
         categories: [
-            {name: 'Cars', route: '/cars'},
-            {name: 'Categorie2', route: '/cat2'},
-            {name: 'Categorie3', route: '/cat3'},
-            {name: 'Categorie4', route: '/cat4'},
-            {name: 'Categorie5', route: '/cat5'},
-            {name: 'Categorie6', route: '/cat6'},
-            {name: 'Categorie7', route: '/cat7'}
+            {name: 'Cars', route: '/cars',src:''},
+            {name: 'Dogs', route: '/cat2',src:''},
+            {name: 'Food', route: '/cat3',src:''},
+            {name: 'Cash', route: '/cat4',src:''},
+            {name: 'Humans', route: '/cat5',src:''},
+            {name: 'Phones', route: '/cat6',src:''},
+            {name: 'Footballs', route: '/cat7',src:''},
+            {name: 'Basketballs',route: '/cat8',src:''},
+            {name: 'Books', route: '/cat9',src:''},
+            {name: 'Trees', route: '/cat10',src:''},
+            {name: 'Cats', route: '/cat11',src:''},
+            {name: 'Traffic Lights', route: '/cat12',src:'/Users/moritz/Downloads/CategoriesPics/traffic-light-solid.svg'},
+            {name: 'Lamps', route: '/cat13',src:''},
+            {name: 'Bridges', route: '/cat14',src:''},
+            {name: 'Guitars', route: '/cat15',src:''},
+            {name: 'Trains', route: '/cat16',src:''},
+            {name: 'Boats', route: '/cat17',src:''},
+            {name: 'Shoes', route: '/cat18',src:''},
+            {name: 'Bicycle', route: '/cat19',src:''},
+            {name: 'Eyes', route: '/cat20',src:''},
         ]
     }
+    
 
 
     render() {
@@ -35,17 +53,27 @@ class Overview extends Component {
         })
 
 
+        
+        
         return(
             <div className={classes.overview}>
                 <h2>Categories</h2>
+                    <ul className={classes.thumbnails}>
                 <Container>
-                    <Row>
-                        {catPreviews}
-                    </Row>
+                        {this.state.categories.map(c => {
+                            return(
+                                <li className={classes.categories}>
+                                    <a href={`imageQueue/${c.name}`} className={classes.thumbnail}>
+                                    <img src= 'https://dummyimage.com/260x200/000/fff' alt= {c.name} />
+                                    </a>
+                                </li>
+                            )
+                        })}
                 </Container>
+                </ul>
             </div>
-
         )
+        // {catPreviews}
     }
 }
 
