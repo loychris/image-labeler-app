b2ee79b6cf370c47d9b6544fccd0cf150950c2dc
Marcus <64657499+mbnbln@users.noreply.github.com>
2020-06-14 21:02:02 +0200

    disabled bootstrap css import

--- frontend/labelr/src/index.js
+++ frontend/labelr/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 // Importing the Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <React.StrictMode>
