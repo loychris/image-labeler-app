3dace13acbe9e3e39b4b85e28c3c9b759203288d
Marcus <64657499+mbnbln@users.noreply.github.com>
2020-06-16 18:34:25 +0200

    draft with bootstrap-NavBar

--- frontend/labelr/src/components/Menu/Menu.js
+++ frontend/labelr/src/components/Menu/Menu.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-// import { Button } from 'react-bootstrap';
-
+// import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import classes from './Menu.module.css';
-
-
 
 class Menu extends Component {
 
     render() {
         return(
-            <ul className={classes.topnav}>
-                {/* <li><Button variant="primary">Primary</Button>{' '}</li> */}
-                {/* <li><button>Default</button></li> */}
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/login'>Login</Link></li>
-                <li className={classes.drpdn}>
-                    <Link to='/' className={classes.dropbtn}>Show &#8595;</Link>
-                    <div className={classes.drpdnContent}>
-                        <Link to='/'>Latest uploads</Link>
-                        <Link to='/'>Most active user</Link>
-                    </div>
-                </li>
-                <li className={classes.user}>Logged in as:<Link className={classes.userLink} to='/'>Testuser One</Link></li>
-            </ul>
+            <Navbar /*className={classes.navbar}*/ bg="light" expand="lg">
+                <Navbar.Brand  href="/">Labelr</Navbar.Brand>
+                <Navbar.Toggle className={classes.navbarbrand} aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <NavDropdown title="Show" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/">User's achievments</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/">Recently uploaded images</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                </Navbar.Collapse>
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>Signed in as: <a href="/">Testuser One</a></Navbar.Text>
+                </Navbar.Collapse>
+            </Navbar>
+
+            // <ul className={classes.topnav}>
+            //     <li><Link to='/'>Home</Link></li>
+            //     <li><Link to='/login'>Login</Link></li>
+            //     <li className={classes.drpdn}>
+            //         <Link to='/' className={classes.dropbtn}>Show &#8595;</Link>
+            //         <div className={classes.drpdnContent}>
+            //             <Link to='/'>Latest uploads</Link>
+            //             <Link to='/'>Most active user</Link>
+            //         </div>
+            //     </li>
+            //     <li className={classes.user}>Logged in as:<Link className={classes.userLink} to='/'>Testuser One</Link></li>
+            // </ul>
         )
     }
 
