2fd2aa28a02c224e3ef78b55f4c8626991c27dff
Antonia <konto_326@tu-berlin.de>
2020-06-19 15:24:53 +0200

    Copied get top n rating users from image.js to users.js. Now it supposedly sends back the usernames and number of labeled images

--- backend/src/routers/image.js
+++ backend/src/routers/image.js
@@ -126,22 +126,6 @@ router.get('/images/next', auth, async  (req, res) => {
 } )
 
 
-// Get top n rating users with most labeled images
-router.get('/users/highscores/:n', async (req, res) => {
-    const n = req.params.n;
-    
-    try {
-	let users = await User.find();
-	if (!users) { res.status(400).send('no users found');}
-	
-	users.sort((a,b) => parseFloat(b.labeledImagesID.length) - parseFloat(a.labeledImagesID.length));
-	if (users.length < n) { res.status(200).send(users.slice(0,user.length));}
-        res.status(200).send(users.slice(0, n)); // sends back whole user objects -> needs to be changed
-    } catch(e) {
-	res(500).send(e);
-    }
-} )
-    
 
 // ------------------------ POST ROUTES ------------------------
 
