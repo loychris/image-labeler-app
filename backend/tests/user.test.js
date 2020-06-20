9929a639338f9b4ec39c6c0675d7269b937a5c5c
Martin <l.martin.a@hotmail.com>
2020-06-20 20:47:11 +0200

    all User-tests -> please review

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -37,3 +37,55 @@ test('Should logout all', async () => {
         .set('Authorization', `Bearer ${labler.tokens[0].token}`).
         send().expect(200);
 })
+
+
+
+
+
+
+test('should sign up a new user', async () => {
+    await request(app).post('/users/').send({
+        email: 'newmail@gmail.com',
+        password: labler.password
+    }).expect(201);
+})
+
+test('should not sign up a new user because empty req', async () => {
+    await request(app).post('/users/').send({
+
+    }).expect(500);
+})
+
+test('should not get users/me because unauthenticated', async () => {
+    await request(app).get('/users/me')
+        .set('Authorization', `Bearer ${labler.tokens[0].token}`)
+        .send({ user: labler }).expect(401);
+})
+
+test('should get /users/me', async () => {
+    await request(app).get('/users/me')
+        .set('Authorization', `Bearer ${labler.tokens[0].token}`)
+        .send({ user: labler }).expect(200);
+})
+
+test('should get user with id', async () => {
+    await request(app).get('/users/' + labler._id)
+        .send().expect(200);
+})
+
+test('should not get user cause user doesnt exist', async () => {
+    await request(app).get('users/invaliduseridstring42069')
+        .send().expect(404);
+})
+
+test('delete own account while authenticated', async () => {
+    await request(app).delete('users/' + labler._id)
+        .set('Authorization', `Bearer ${labler.tokens[0].token}`)
+        .send().expect(201);
+})// test delete other account issue isnt there and not sure if i should test for that
+
+test('delete own account without authentication', async () => {
+    await request(app).delete('users/' + labler._id)
+        .send().expect(401);
+})
+
