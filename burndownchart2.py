9cbc92e57af4969449ecb28bab8c167e23f3b837
Antonia <konto_326@tu-berlin.de>
2020-06-22 11:26:15 +0200

    Takes 3 arguments, filename, number of columns, number of graphs. Plots the ideal and the actual graph and, if the user wishes, an actual graph with the time that was actually used

--- burndownchart2.py
+++ burndownchart2.py
@@ -2,58 +2,76 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
+if len(sys.argv) == 2:
+    sys.argv.append("3")
 
 days = ["fr", "sa", "so", "mo", "di", "mi", "do"] 
-
 sprintdays = dict(zip(days, [1,2,3,4,5,6,7]))
 burnt_minutes = [0]*8
+burnt_actualminutes = [0]*8
 sum_minutes = 0
 issues = []
+day_in_minutes = 480
 
-with open ('Sprint1_issues-minutes-closed.txt') as f:
+with open (sys.argv[1]) as f:
     for line in f:
-        issue, minute_str, daynewline = line.split(" ")  
-        minutes = int(minute_str)
+        if sys.argv[2] == "4":
+            issue, minutes_str, day, actualminutes_newline = line.split(" ")  
+            actualminutes = int(actualminutes_newline.rstrip("\n"))
+        if sys.argv[2] == "3":
+            issue, minutes_str, daynewline = line.split(" ")  
+            day = daynewline.rstrip("\n")
+        minutes = int(minutes_str)
         sum_minutes += minutes
-        day = daynewline.rstrip("\n")
         issues.append(issue)
         if day == "none":
             continue
-        burnt_minutes[sprintdays[day]]+= minutes
+        burnt_minutes[sprintdays[day]] += minutes
+        burnt_actualminutes[sprintdays[day]] += minutes
 
-burndown = [sum_minutes]*8
-print(burndown)
-for i in range(len(burndown)):
-    burndown[i]-= sum(burnt_minutes[:i+1])
+workload_week = [sum_minutes]*8
 
-day_in_minutes = 480
-#hour = 60
-tasks_in_days = sum_minutes/day_in_minutes
-#tasks_in_hours = sum_minutes/hour
-burnt_days = [item/day_in_minutes for item in burndown]
-#burnt_hours = [item/hour for item in burndown]
+def burn_minutes(workload_week, burndown_week):
+    burnt = 0
+    for i in range(len(workload_week)):
+        burnt += burndown_week[i]
+        workload_week[i] -= burnt
+    return workload_week
+
+def minutes2days(burnt_week):
+    return [item/day_in_minutes for item in burnt_week]
+
+burntdown = burn_minutes(workload_week, burnt_minutes)
+burnt_days = minutes2days(burntdown)
 burnt_days_arr = np.array(burnt_days)
-#burnt_hours_arr = np.array(burnt_hours)
 
-#sum_in_hours = sum_minutes/7
+tasks_in_days = sum_minutes/day_in_minutes
+
 x = np.arange(0,8,1)
-#x = np.arange(0, sum_in_hours+1,1)
 y = np.arange(tasks_in_days)
-#y = np.arange(tasks_in_hours)
 plt.plot([0,7], [tasks_in_days,0], label = 'Ideal Tasks Remaining')
-#plt.plot([0,sum_in_hours], [tasks_in_hours,0], label = 'Ideal Tasks Remaining')
 plt.axis('equal')
 plt.plot(x, burnt_days_arr, label = "Actual Tasks Remaining")
-#plt.plot(x, burnt_hours_arr, label = 'Actual Tasks Remaining')
+
+if sys.argv[2] == "4":
+    print("Third argument is 4")
+    if sys.argv[3] == "3":
+        print("Fourth argument is 3")
+        burntdown_actual = burn_minutes(workload_week, burnt_actualminutes)
+        burnt_days_actual = minutes2days(burntdown_actual)
+        burnt_days_actual_arr = np.array(burnt_days_actual)
+        plt.plot(x, burnt_days_actual_arr, label = "Actual Time Burnt")
 
 ax = plt.subplot(1,1,1)
-ax.set_ylim(bottom = 0.)
-
+#ax.set_xlim(xmin=0)
+ax.spines['left'].set_position('zero')
+ax.axhline(y=0, color='k')
+#ax.axvline(x=0)
 
 plt.xlabel('Time (in days)')
-plt.ylabel('Tasks (in days)')
-#plt.ylabel('Tasks (in hours)')
-plt.title('Burn-down Chart Sprint 1')
+plt.ylabel('Tasks (in days)\n56h = 7 Tage')
+plt.title('Burn-down Chart Sprint 2')
 plt.legend()
 plt.show()
