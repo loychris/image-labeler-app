7d3d4e13e42b63039d1ab3aa37d56816adae4605
Chris Loy <cloy202@gmail.com>
2020-07-05 16:40:27 +0200

    Adde dummy state to Highscore

--- frontend/labelr/src/components/Highscore/Highscore.js
+++ frontend/labelr/src/components/Highscore/Highscore.js
@@ -3,6 +3,19 @@ import classes from './Highscore.module.css';
 
 class Highscore extends Component {
 
+    state = {
+        topUsers = [
+            {
+                username: "Marcus",
+                imagesLabeled: 3210
+            },
+            {
+                username: "Chris",
+                imagesLabeled: 2200
+            }
+        ]
+    }
+
     render(){
         return(
             <main className={classes.Highscore}>
