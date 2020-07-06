b5e916add485b08010725b6062f505742446e501
Marcus <64657499+mbnbln@users.noreply.github.com>
2020-07-06 22:53:09 +0200

    created highscore component

--- frontend/labelr/src/components/Highscore/Highscore.js
+++ frontend/labelr/src/components/Highscore/Highscore.js
@@ -5,21 +5,74 @@ class Highscore extends Component {
   state = {
     topUsers: [
       {
-        username: 'Marcus',
-        imagesLabeled: 3210,
+        ranking: 1,
+        username: 'Martin',
+        imagesLabeled: 1100,
+        completedCategories: 36,
       },
       {
+        ranking: 2,
         username: 'Chris',
         imagesLabeled: 2200,
+        completedCategories: 13,
+      },
+      {
+        ranking: 3,
+        username: 'Tamir',
+        imagesLabeled: 5500,
+        completedCategories: 21,
+      },
+      {
+        ranking: 4,
+        username: 'Pascal',
+        imagesLabeled: 9000,
+        completedCategories: 110,
+      },
+      {
+        ranking: 5,
+        username: 'Antonia',
+        imagesLabeled: 15500,
+        completedCategories: 10,
+      },
+      {
+        ranking: 6,
+        username: 'Moritz',
+        imagesLabeled: 15500,
+        completedCategories: 10,
+      },
+      {
+        ranking: 7,
+        username: 'Marcus',
+        imagesLabeled: 3210,
+        completedCategories: 12,
       },
     ],
   };
+
+  generateTable() {
+    return this.state.topUsers.map((topUsers) => (
+      <tr>
+        <td>{topUsers.ranking}</td>
+        <td>{topUsers.username}</td>
+        <td>{topUsers.completedCategories}</td>
+        <td>{topUsers.imagesLabeled}</td>
+      </tr>
+    ));
+  }
 
   render() {
     return (
       <main className={classes.Highscore}>
         <h1>Highscore</h1>
-        <hr />
+        <table>
+          <tr>
+            <th>Ranking</th>
+            <th>Username</th>
+            <th>Completed Categories</th>
+            <th>Images Labeled</th>
+          </tr>
+          {this.generateTable()}
+        </table>
       </main>
     );
   }
