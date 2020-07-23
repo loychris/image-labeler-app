689c8593174420a3bc311bdbee4ed79f161a7bd4
morixn <64641295+morixn@users.noreply.github.com>
2020-07-23 19:10:28 +0200

    added new achievements

--- frontend/labelr/src/components/Achievements/Achievements.js
+++ frontend/labelr/src/components/Achievements/Achievements.js
@@ -45,7 +45,9 @@ render() {
     // Number that represents the current Date -100 days
     const hundredDaysago = parseInt(moment().subtract(100, 'day').format().substr(0,10).replace('-', '').replace('-', ''))
 
-    // Nuber that represents the current date
+    //Number that represents the current Date -365 days
+
+    //Number that represents the current date
     const currentMoment = parseInt(moment().format().substr(0,10).replace('-', '').replace('-', ''))
 
     // total number of images labeled by user
@@ -58,7 +60,6 @@ render() {
     const achievements = [ 
         {name: 'The First of Many!' , description: 'First Image labeled',src: pic2, progress: counter > 0 ? 100 : 0},
         {name: 'Beginner' , description: '10 Images labeled',src: pic5, progress: counter > 9 ? 100 : counter/10 * 100},
-        // // {name: 'Getting the hang of it' , description: '50 Images labeled',src:pic7},
         {name: 'Newcomer' , description: '100 Images labeled',src:pic7, progress: counter > 99 ? 100 : counter},
         {name: 'Mom would be proud' , description: '500 Images labeled',src:pic8, progress: counter > 499 ? 100 : counter/500 * 100},
         {name: 'You are a Winner!' , description: '1000 Images labeled',src:pic9, progress: counter > 999 ? 100 : counter/1000 * 100},
@@ -67,7 +68,6 @@ render() {
         {name: 'Cyborg' , description: '20000 Images labeled',src:pic3, progress: counter > 19999 ? 100 : counter/20000 * 100},
         {name: 'At the top' , description: '50000 Images labeled',src:pic20, progress: counter > 49999 ? 100 : counter/50000 * 100},
         {name: 'Master of the universe' , description: '100000 Images labeled',src:pic11, progress: counter > 100000 ? 100 : counter/100000 * 100},
-        // {name: 'They wont label themselfs' , description: '50 Images labeled 1 in one day',src:pic7},
         {name: 'Too Fast!' , description: '100 Images labeled 1 in one day',src:pic12, progress: counter > 99 ? 100 : counter},
         {name: 'Hustler' , description: '500 Images labeled in 1 week',src:pic19,progress: counterThisWeek > 499 ? 100 : counterThisWeek/500*100},
         {name: 'Work work work' , description: '1000 Images labeled in 1 week',src:pic15,progress:counterThisWeek > 999 ? 100 : counterThisWeek/1000*100},
@@ -78,28 +78,10 @@ render() {
         {name: 'Explorer' , description: '2000 Images labeled in 1 month',src:pic6,progress:counterThisMonth > 1999 ? 100 : counterThisMonth/2000*100},
         {name: 'Hell of a month!' , description: '5000 Images labeled in 1 month',src:pic17,progress:counterThisMonth > 4999 ? 100 : counterThisMonth/5000*100},
         {name: 'Unstoppable Force' , description: '10000 Images labeled in 1 month',src:pic16,progress:counterThisMonth > 9999 ? 100 : counterThisMonth/10000*100},
-        // {name: 'Becoming a regular' , description: '100+ Images labeled 5 Days in a row',src:pic7},
-        // {name: 'Strike!' , description: '100+ Images labeled 10 days in a row',src:pic7},
-        // {name: 'Part of my routine!' , description: '100+ Images labeled 15 days in a row',src:pic7},
-        // {name: 'Can´t live without' , description: '100+ Images labeled 25 days in a row',src:pic7},
-        // {name: 'What even is heroin?' , description: '100+ Images labeled 30 days in a row',src:pic10, progress: 0},
-        // {name: 'Allrounder' , description: '100+ Images labeled in 20 different categories',src:pic7},
-        // {name: 'I´ve seen it all!' , description: '200+ Images labeled in 50 different categories',src:pic7},
         {name: 'A new moon!' , description: '1 month on plattform',src:pic1, progress: createdAt < hundredDaysago ? 100 : 0},
-        // {name: 'Back in my age..!' , description: '24 active month since signup',src:pic7},
-        // {name: 'Veteran' , description: '60 active month on plattform',src:pic7},
-
-        //100+ Images labeled in 5 different categories
-        //100+ Images labeled in 10 different categories
-
-        //6 active month since signup
-        //12 active month since signup
+        {name: '1 YEAR!' , description: '1 year on plattform',src:pic1, progress: createdAt < hundredDaysago ? 100 : 0},
+        {name: '2 YEARS!' , description: '1 month on plattform',src:pic1, progress: createdAt < hundredDaysago ? 100 : 0},
     ]
-
-
-    
-
-    
 
     const achPreviews = achievements.map(c => {
         return <AchPreview {...c} />
