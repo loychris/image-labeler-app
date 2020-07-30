e18511a71d643ab6520ce7aa1fede2abf21fbbb1
Chris Loy <cloy202@gmail.com>
2020-07-30 07:04:43 +0200

    whatever

--- frontend/labelr/src/components/Overview/CatPreview/CatPreview.js
+++ frontend/labelr/src/components/Overview/CatPreview/CatPreview.js
@@ -16,7 +16,6 @@ class CatPreview extends Component {
                     <div className={classes.thumbnail}>
                         <img className={classes.Img} src={this.props.src} alt={this.props.name} />
                         <span className={classes.caption}>{this.props.name}</span>
-                        <div className={classes.time}>{this.props.time}</div>
                     </div>
                     {/* <ProgressBar className={classes.progressBar} variant="success" now={this.props.progress} label={`${Math.floor(this.props.progress)}%`} />
                     <div className= {classes.progress}>{this.props.progress}</div> */}
